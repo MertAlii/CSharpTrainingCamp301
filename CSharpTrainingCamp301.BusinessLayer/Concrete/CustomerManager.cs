3d8c17b5f30cda4b5998b5c3f8d4fea79236735d
MertAlii <Aslancocukmz@gmail.com>
2025-07-13 22:18:28 +0300

    C# Bootcamp Lesson 19 - Business Layer and Logic Rules

---
+++ CSharpTrainingCamp301.BusinessLayer/Concrete/CustomerManager.cs
@@ -0,0 +1,60 @@
+﻿using CSharpTrainingCamp301.BusinessLayer.Abstract;
+using CSharpTrainingCamp301.DataAccessLayer.Abstract;
+using CSharpTrainingCamp301.EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpTrainingCamp301.BusinessLayer.Concrete
+{
+    public class CustomerManager : ICustomerService
+    {
+        private readonly ICustomerDal _customerDal;
+
+        public CustomerManager(ICustomerDal customerDal)
+        {
+            _customerDal = customerDal;
+        }
+
+        public void TDelete(Customer entity)
+        {
+            _customerDal.Delete(entity);
+        }
+
+        public List<Customer> TGetAll()
+        {
+            //if(Yetki varsa)
+            return _customerDal.GetAll();
+        }
+
+        public Customer TGetById(int id)
+        {
+            return _customerDal.GetById(id);
+        }
+
+        public void TInsert(Customer entity)
+        {
+            if (entity.CustomerName != "" && entity.CustomerName.Length >= 3 && entity.CustomerCity != null && entity.CustomerSurname != "" && entity.CustomerName.Length <= 30)
+            {
+               _customerDal.Insert(entity);
+            }
+            else
+            {
+                // Hata mesajı ver
+            }
+        }
+
+        public void TUpdate(Customer entity)
+        {
+            if (entity.CustomerId!=0 && entity.CustomerCity.Length >=3)
+            {
+                _customerDal.Update(entity);
+            }
+            else
+            {
+                // Hata mesajı ver
+            }
+        }
+}
